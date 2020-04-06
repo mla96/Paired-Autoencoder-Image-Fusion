8752dc27ab374d86c174a433ae8a35e925f2660d
Michelle La <mal928@nyu.edu>
2020-04-06 16:28:58 -0400

    Add loss to plot titles in plot_tensors_tensorboard

--- AutoEncoder/tensorboard_functions.py
+++ AutoEncoder/tensorboard_functions.py
@@ -1,10 +1,15 @@
+import os
 from matplotlib import pyplot as plt
 plt.switch_backend('agg')
 
 
-def plot_tensors_tensorboard(input, output, step, writer):
+def plot_tensors_tensorboard(input, output, step, epoch, loss, writer, output_path):
+    # Create plot path directory if it doesn't exist
+    if not os.path.exists(output_path):
+        os.mkdir(output_path)
+
     fig = plt.figure()
-    plt.title("Step " + str(step), y=0.9)
+    plt.title("Step " + str(step) + "; Epoch: {}".format(epoch + 1) + "\n Loss: {:.5f}".format(loss), y=0.9)
     plt.axis('off')
     fig.add_subplot(1, 2, 1)
     plt.imshow(input)
@@ -12,5 +17,6 @@ def plot_tensors_tensorboard(input, output, step, writer):
     fig.add_subplot(1, 2, 2)
     plt.imshow(output)
     plt.axis('off')
+    plt.savefig(os.path.join(output_path, "trainfig_step" + str(step)))
     writer.add_figure('train_fig', fig, step)
     print('\n Figure added \n')
