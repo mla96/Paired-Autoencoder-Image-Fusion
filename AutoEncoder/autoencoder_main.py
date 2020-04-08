6db47502670785dbc8dd6485db24721acbab1379
Michelle La <mal928@nyu.edu>
2020-04-07 22:03:57 -0400

    Add comments

--- AutoEncoder/autoencoder_main.py
+++ AutoEncoder/autoencoder_main.py
@@ -1,42 +1,53 @@
+#!/usr/bin/env python3
+"""
+This file is the main script to train an AutoEncoder on a square image set. The training set is a collection of publicly
+available RGB fundus images that have been appropriately resized and cropped.
+This file includes calling the AutoEncoder model with hyperparameters, training, and validation.
+
+"""
+
+
 import albumentations as A
-import torch.multiprocessing
 from PIL import ImageFile
 
 from autoencoder import *
 from autoencoder_datasets import ImbalancedDataset, UnlabeledDataset
-from autoencoder_traintest_functions import train, test, tensors_to_images
+from autoencoder_traintest_functions import *
 
+import torch.multiprocessing
 from torch.utils.tensorboard import SummaryWriter
+
+
+# Paths to source data, validation data, and output saves
+transfer_data_path = "../../../../OakData/Transfer_learning_datasets"
+data_paths = [os.path.join(transfer_data_path, "train"),
+              os.path.join(transfer_data_path, "KaggleDR_crop")]
+data_paths_AMD = [os.path.join(transfer_data_path, "train_AMD")]
+valid_data_path = [os.path.join(transfer_data_path, "validation")]  # Contains small data set for validation
+save_model_path = "../../FLIO-Thesis-Project/AutoEncoder/AutoEncoder_Results"
+model_base_name = "autoencoder"
+
+# Training parameters
+model_architecture = "noRes"  # Options: noRes, Res34
+epoch_num = 300
+train_data_type = "AMDonly"  # Options: None, AMDonly
+loss_type = "L1"  # Options: L1, MSE
+batch_size = 20
+num_workers = 12
+plot_steps = 200  # Number of steps between getting random input/output to show training progress
+stop_condition = 10000  # Number of steps without improvement for early stopping
+
+overwrite = False  # Overwrite existing model to train again
 
 
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 torch.multiprocessing.set_sharing_strategy('file_system')
 
-transfer_data_path = "../../../../OakData/Transfer_learning_datasets"
-data_paths = [os.path.join(transfer_data_path, "train"),
-              os.path.join(transfer_data_path, "KaggleDR_crop")]
-data_paths_AMD = [os.path.join(transfer_data_path, "train_AMD")]
-valid_data_path = [os.path.join(transfer_data_path, "validation")]
-save_model_path = "../../FLIO-Thesis-Project/AutoEncoder/AutoEncoder_Results"
-model_base_name = "autoencoder_imbweight"
-
-# Training parameters
-model_architecture = "Res34"  # Options: noRes, Res34
-epoch_num = 50
-train_data_type = "AMDonly"  # Options: None, AMDonly
-batch_size = 20
-num_workers = 12
-plot_steps = 50  # Number of steps between getting random input/output to show training progress
-stop_condition = 10000  # Number of steps without improvement for early stopping
-
-
-overwrite = True
-
 model_name_pt1 = model_base_name + "_" + model_architecture + "_" + str(epoch_num) + "e_"
 if train_data_type:
-    model_name = model_name_pt1 + train_data_type + "_batch" + str(batch_size) + "_workers" + str(num_workers)
+    model_name = model_name_pt1 + train_data_type + "_" + loss_type + "_batch" + str(batch_size) + "_workers" + str(num_workers)
 else:
-    model_name = model_name_pt1 + "batch" + str(batch_size) + "_workers" + str(num_workers)
+    model_name = model_name_pt1 + "_" + loss_type +  "batch" + str(batch_size) + "_workers" + str(num_workers)
 
 n_channels = 3
 
@@ -58,23 +69,26 @@ if not os.path.exists(valid_output_path):
 w = 0.995  # weight of rare event
 sample_weights = [1 - w, w]
 augmentation_pipeline = A.Compose(
-    [A.HorizontalFlip(p=0.5), A.VerticalFlip(p=0.5), A.RandomRotate90(p=0.5), A.RandomCrop(128, 128, p=1.0)]
+    [A.HorizontalFlip(p=0.5), A.VerticalFlip(p=0.5),
+     A.RandomRotate90(p=0.5),
+     A.RandomCrop(128, 128, p=1.0)]
 )
 
 unlabeled_dataset = ImbalancedDataset(data_paths_AMD, augmentations=augmentation_pipeline)
 dataloader = DataLoader(unlabeled_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers, pin_memory=True)
 
 
+# Define model with hyperparameters
 if model_architecture == "Res34":
     model = AutoEncoder_ResEncoder(n_channels=n_channels,
                         n_decoder_filters=[128, 64, 32, 32],
-                        trainable=False).to(device)
+                        trainable=True).to(device)
     model.apply(AutoEncoder_ResEncoder.init_weights)  # initialize model parameters with normal distribution
 else:  # noRes
     model = AutoEncoder(n_channels=n_channels,
                         n_encoder_filters=[32, 64, 128, 256, 256],
                         n_decoder_filters=[128, 64, 32, 32],
-                        trainable=False)  #.to(device)
+                        trainable=True).to(device)
     model.apply(AutoEncoder.init_weights)  # Initialize weights
 
 
@@ -83,12 +97,16 @@ if torch.cuda.device_count() > 1:
 print("Now using", torch.cuda.device_count(), "GPU(s) \n")
 model.to(device)
 
-criterion = nn.MSELoss().to(device)
+# Select loss
+if loss_type == "MSE":
+    criterion = nn.MSELoss().to(device)
+else:  # L1
+    criterion = nn.L1Loss().to(device)
 optimizer = optim.Adam(model.parameters(), lr=0.0001)
 # optimizer = optim.SGD(model.parameters(), lr=0.0001, momentum=0.9)
 scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', factor=0.8)
 
-# TRAINING
+# Training
 save_model_path_filename = os.path.join(model_output_path, model_name + '.pth')
 if not os.path.isfile(save_model_path_filename) or overwrite:
     tensorboard_writer = SummaryWriter(model_output_path)
@@ -97,6 +115,7 @@ if not os.path.isfile(save_model_path_filename) or overwrite:
     # tensorboard_writer.add_graph(model, input_to_model=(image,), verbose=True)
 
     train(model, dataloader, epoch_num, criterion, optimizer, scheduler, device, tensorboard_writer,
+          os.path.join(save_model_path, model_name, 'Figures'),
           plot_steps=plot_steps, stop_condition=stop_condition, sample_weights=sample_weights)
     torch.save(model.state_dict(), save_model_path_filename)
 
@@ -105,7 +124,7 @@ if not os.path.isfile(save_model_path_filename) or overwrite:
 else:
     model.load_state_dict(torch.load(save_model_path_filename))
 
-# TESTING
+# Testing
 validation_dataset = ImbalancedDataset(valid_data_path)
 valid_dataloader = DataLoader(validation_dataset, batch_size=1, shuffle=True, num_workers=12)
 
@@ -115,4 +134,5 @@ print(valid_outputs[0])
 print("Validation losses")
 print(valid_losses[0])
 
+# Save outputs as JPEG images
 tensors_to_images(valid_outputs, valid_filenames, valid_output_path)
