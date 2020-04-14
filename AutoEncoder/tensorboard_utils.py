308d3a3a97726d512ade9c912af4c99b3a29e3d4
Michelle La <mal928@nyu.edu>
2020-04-14 19:50:10 -0400

    Create function add_image_tensorboard with documentation for managing plot_tensors_tensorboard and appropriately changing model train/eval status

--- AutoEncoder/tensorboard_utils.py
+++ AutoEncoder/tensorboard_utils.py
@@ -15,19 +15,50 @@ from matplotlib import pyplot as plt
 plt.switch_backend('agg')
 
 
+def add_image_tensorboard(model, image, **kwargs):
+    """
+    Parameters
+    ---
+    model: PyTorch model
+    image: input tensor
+    **kwargs: to be fed directly into plot_tensors_tensorboard
+        1. step
+        2. epoch
+        3. loss
+        4. writer
+        5. output_path
+    """
+    model.eval()  # Change to evaluation mode to test the model
+    output = model(image)
+
+    randint = np.random.randint(0, image.size()[0])
+    input_im = image[randint].detach().cpu().numpy()
+    output_im = output[randint].detach().cpu().numpy()
+    input_im = denormalize_and_rescale(np.transpose(input_im, (1, 2, 0)))
+    output_im = denormalize_and_rescale(np.transpose(output_im, (1, 2, 0)))
+    plot_tensors_tensorboard(input_im, output_im, *kwargs.values())
+
+    model.train()  # Change back to train for backpropagation
+
+
 def plot_tensors_tensorboard(input, output, step, epoch, loss, writer, output_path):
     if not os.path.exists(output_path):  # Create plot path directory if it doesn't exist
         os.mkdir(output_path)
 
     fig = plt.figure()
-    plt.title("Step " + str(step) + "; Epoch: {}".format(epoch + 1) + "\n Loss: {:.5f}".format(loss), y=0.9)
+    plt.title(f"Step {step}; Epoch: {epoch + 1} \n Loss: {loss}", y=0.9)
     plt.axis('off')
     fig.add_subplot(1, 2, 1)
-    plt.imshow((input * 255).astype(np.uint8))
+    plt.imshow(input.astype(np.uint8))
     plt.axis('off')
     fig.add_subplot(1, 2, 2)
-    plt.imshow((output * 255).astype(np.uint8))
+    plt.imshow(output.astype(np.uint8))
     plt.axis('off')
     plt.savefig(os.path.join(output_path, "trainfig_step" + str(step)))
     writer.add_figure('train_fig', fig, step)
     print('\n Figure added \n')
+
+
+def denormalize_and_rescale(image):
+    image = 0.5 * image + 0.5  # denormalize
+    return 255 * image  # rescale
