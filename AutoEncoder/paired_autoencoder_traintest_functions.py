2d331d2834c1460e8a436d52d1fd1fe65ee9dc7d
Michelle La <mal928@nyu.edu>
2020-04-18 21:17:17 -0400

    Adapt train/test functions to PairedUnlabeledDataset; add CosineSimilarity loss calculation to training loop

--- AutoEncoder/paired_autoencoder_traintest_functions.py
+++ AutoEncoder/paired_autoencoder_traintest_functions.py
@@ -1,80 +1,56 @@
-import os
-
 import numpy as np
-import torch
+import torch.nn as nn
 from torchvision.transforms import transforms
+
+import hdf5storage
+import skimage.io
+from loss_utils import *
+from autoencoder_traintest_functions import StopCondition
 from matplotlib import pyplot as plt
 plt.switch_backend('agg')
 
 
-class StopCondition:
-    def __init__(self, patience):
-        self.patience = patience  # How many steps to evaluate model improvement
-        self.min_loss = float('inf')
-        self.stop = False
-        self.patience_step = 0
-
-    def evaluate_stop(self, loss):
-        if loss < self.min_loss:
-            self.min_loss = loss
-            self.patience_step = 0
-        else:
-            self.patience_step += 1
-            if self.patience_step >= self.patience:
-                self.stop = True
-
-
-def mutual_information_loss(joint, marginal, mine_net):
-    t = mine_net(joint)
-    et = torch.exp(mine_net(marginal))
-    mi_lb = torch.mean(t) - torch.log(torch.mean(et))
-    return mi_lb, t, et
-
-
-def learn_mine(batch, mine_net, mine_net_optim, ma_et, ma_rate=0.01):
-    # batch is a tuple of (joint, marginal)
-    joint, marginal = batch
-    joint = torch.autograd.Variable(torch.FloatTensor(joint)).cuda()
-    marginal = torch.autograd.Variable(torch.FloatTensor(marginal)).cuda()
-    mi_lb, t, et = mutual_information(joint, marginal, mine_net)
-    ma_et = (1 - ma_rate) * ma_et + ma_rate * torch.mean(et)
-
-    # unbiasing use moving average
-    loss = -(torch.mean(t) - (1 / ma_et.mean()).detach() * torch.mean(et))
-    # use biased estimator
-    #     loss = - mi_lb
-
-    mine_net_optim.zero_grad()
-    autograd.backward(loss)
-    mine_net_optim.step()
-    return mi_lb, ma_et
-
-
-def train(model, trainloader, epoch_num, criterion, optimizer, scheduler, device, writer,
-          plot_steps=1000, stop_condition=4000, sample_weights=None):
+def train(model, model2, trainloader, epoch_num, criterion, optimizer, scheduler, device, writer, output_path,
+          plot_steps=1000, stop_condition=4000):
     model.train()
+    model2.train()
     if stop_condition:
         stop_condition = StopCondition(stop_condition)
     epoch_log = open("log.txt", "a")
 
     for epoch in range(epoch_num):
         running_loss = 0
-        # file_labelweight is a tuple containing the file basename and a weight label
-        for i, (image, target, file_labelweight) in enumerate(trainloader):
+        for i, (image, image2, target, target2, image_files) in enumerate(trainloader):
+            optimizer.zero_grad()
             image, target = image.to(device), target.to(device)
-            output = model(image)
+            features, output = model(image)
+            if isinstance(criterion, SSIM_Loss) or isinstance(criterion, MS_SSIM_Loss):
+                output, target = denormalize(output), denormalize(target)
             loss = criterion(output, target)
+
+            image2, target2 = image2.to(device), target2.to(device)
+            features2, output2 = model2(image2)
+            loss2 = criterion(output2, target2)
+
+            features = features.view(1, 8192)
+            features2 = features2.view(1, 8192)
+
+            latent_corr = nn.CosineSimilarity(dim=1)  # Resize into 1 dimensional array to get a single value?
+            cos_loss = latent_corr(features, features2)
+            total_loss = loss + loss2 + cos_loss
+
             step = i + epoch * len(trainloader)
 
-            if isinstance(file_labelweight, list) and sample_weights:
-                _, label = file_labelweight
-                sample_weight = torch.tensor([sample_weights[l] for l in label]).cuda()
-                loss = custom_mse_loss(output, target, sample_weight)
-            loss.backward()
+            total_loss.backward()
             optimizer.step()
 
-            running_loss += loss.item()
-            print(i)
+            running_loss += total_loss.item()
+            if i % 100 == 0:
+                print(i)
+            if step % plot_steps == 0:  # Generate training progress reconstruction figures every # steps
+                add_image_tensorboard(model, image, step=step, epoch=epoch, avg_loss=running_loss / len(trainloader),
+                                      writer=writer, output_path=output_path)
+
             if i % len(trainloader) == len(trainloader) - 1:
                 print('[Epoch: {}, i: {}] loss: {:.5f}'.format(epoch + 1, i + 1, running_loss / len(trainloader)))
                 # Tensorboard
@@ -87,26 +63,9 @@ def train(model, trainloader, epoch_num, criterion, optimizer, scheduler, device
 
                 running_loss = 0
 
-            if step % plot_steps == 0:  # Generate training progress reconstruction figures every # steps
-                fig = plt.figure()
-                randint = np.random.randint(0, image.size()[0])
-                input_im = image.detach().cpu().numpy()[randint]
-                output_im = output.detach().cpu().numpy()[randint]
-                input_im, output_im = np.transpose(input_im, (1, 2, 0)), np.transpose(output_im, (1, 2, 0))
-                plt.title("Step " + str(step), y=0.9)
-                plt.axis('off')
-                fig.add_subplot(1, 2, 1)
-                plt.imshow(input_im)
-                plt.axis('off')
-                fig.add_subplot(1, 2, 2)
-                plt.imshow(output_im)
-                plt.axis('off')
-                writer.add_figure('train_fig', fig, step)
-                print('\n Figure added \n')
-
-        torch.save(model.state_dict(), 'checkpoint.pth')
+        # torch.save(model.state_dict(), 'checkpoint.pth')
         epoch_log.write('Epoch: ' + str(epoch))
-        scheduler.step(loss)
+        scheduler.step(total_loss)
     epoch_log.close()
 
 
@@ -116,15 +75,20 @@ def test(model, testloader, criterion, device):
     losses = []
     filenames = []
     with torch.no_grad():
-        for image, target, file_labelweight in testloader:
+        for i, (image, image2, target, target2, image_files) in enumerate(testloader):
             image, target = image.to(device), target.to(device)
             output = model(image)
             outputs.append(output)
+
+            if isinstance(criterion, SSIM_Loss) or isinstance(criterion, MS_SSIM_Loss):
+                output, target = denormalize(output), denormalize(target)
             losses.append(criterion(output, target))
-            if isinstance(file_labelweight, list):
-                filenames.append(file_labelweight[0])  # appends file_name
-            else:
-                filenames.append(file_labelweight)
+
+            filenames.append(image_files[0])
+            # if isinstance(file_labelweight, list):
+            #     filenames.append(file_labelweight[0])  # appends file_name
+            # else:
+            #     filenames.append(file_labelweight)
 
     return outputs, losses, filenames
 
