9f89225b18541209d9f41a7158cf1378a008cc04
Michelle La <mal928@nyu.edu>
2021-02-04 21:07:40 -1000

    Add MEF-SSIM losses

--- AutoEncoder/loss_utils.py
+++ AutoEncoder/loss_utils.py
@@ -17,6 +17,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from ssim import SSIM, MS_SSIM
+from mefssim import MEFSSIM, MEF_MSSSIM
 
 
 def get_loss_type(type, channel=3):
@@ -26,7 +27,10 @@ def get_loss_type(type, channel=3):
                   'MS-SSIM': MS_SSIM_Loss(data_range=1, nonnegative_ssim=True, win_size=11, channel=channel,
                                           K=(0.01, 0.03)),
                   'MS-SSIM-L1': MS_SSIM_L1_Loss(l1_weight=0.1, data_range=1, nonnegative_ssim=True, win_size=11,
-                                                channel=channel, K=(0.01, 0.03))}
+                                                channel=channel, K=(0.01, 0.03)),
+                  'MEFSSIM': MEFSSIM_Loss(),
+                  'MEF-MSSSIM': MEFSSIM_Loss(),
+                  'MEF-MSSSIM-L1': MEF_MSSSIM_L1_Loss(l1_weight=0.1)}
     return loss_types.get(type)
 
 
@@ -64,6 +68,27 @@ class MS_SSIM_L1_Loss(MS_SSIM):
         ms_ssim_loss = 1 - super(MS_SSIM_L1_Loss, self).forward(img1, img2)
         l1_loss = F.l1_loss(img1, img2)
         return ms_ssim_loss + self.l1_weight * l1_loss
+
+
+class MEFSSIM_Loss(MEFSSIM):
+    def forward(self, img1, img2):
+        return 1 - super(MEFSSIM_Loss, self).forward(img1, img2)
+
+
+class MEF_MSSSIM_Loss(MEF_MSSSIM):
+    def forward(self, img1, img2):
+        return 1 - super(MEF_MSSSIM_Loss, self).forward(img1, img2)
+
+
+class MEF_MSSSIM_L1_Loss(MEF_MSSSIM):
+    def __init__(self, l1_weight=0.1, **kwargs):
+        super(MEF_MSSSIM_L1_Loss, self).__init__(**kwargs)
+        self.l1_weight = l1_weight
+
+    def forward(self, img1, img2):
+        mef_ssim_loss = 1 - super(MEF_MSSSIM_L1_Loss, self).forward(img1, img2)
+        l1_loss = F.l1_loss(img1, img2)
+        return mef_ssim_loss + self.l1_weight * l1_loss
 
 
 def mutual_information_calc(t, et):
